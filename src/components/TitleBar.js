0d53bc0b4fc0523d9ba97b3e43f23fa0280467cf
YashSinghChauhan-27 <yc3343@srmist.edu.in>
2024-07-09 22:42:44 +0530

    titlebar updated

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,10 +1,7 @@
 import React from "react";
-import newsdawglogo from "../components/newsdawglogo.png";
-import decoration from "../components/decoration.png";
-
+import newsdawglogo from "../components/titlelogo.png";
 const TitleBar = () => {
   const titleBarStyle = {
-    backgroundColor: "#350a58",
     color: "white",
     textAlign: "center",
     padding: "20px 0",
@@ -15,12 +12,7 @@ const TitleBar = () => {
 
   return (
     <div style={titleBarStyle}>
-      <h1 style={{ fontSize: "100px" }}>
-        <img
-          src={decoration}
-          alt="Newsdawg Logo"
-          style={{ verticalAlign: "middle", height: "50px", width: "auto", marginBottom: '20px' }}
-        />
+      <h1 style={{ fontSize: "100px", color:"black" }}>
         {" "}NEWS{"   "}
         <img
           src={newsdawglogo}
@@ -28,11 +20,6 @@ const TitleBar = () => {
           style={{ verticalAlign: "middle", height: "100px", width: "auto", marginBottom: '20px' }}
         />{"   "}
         DA<span style={{ color: "#FC5607" }}>W</span>G{" "}
-        <img
-          src={decoration}
-          alt="Newsdawg Logo"
-          style={{ verticalAlign: "middle", height: "50px", width: "auto", marginBottom: '20px' }}
-        />
       </h1>
     </div>
   );
